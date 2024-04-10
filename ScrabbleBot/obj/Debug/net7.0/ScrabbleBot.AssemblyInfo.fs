ecc6eb03b1fdf949a9b64fe57fb9c296ecc9eafe
Mikkel Toft <mikto@itu.dk>
2024-04-10 12:35:53 +0200

    why tgis

--- ScrabbleBot/obj/Debug/net7.0/ScrabbleBot.AssemblyInfo.fs
+++ ScrabbleBot/obj/Debug/net7.0/ScrabbleBot.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("ScrabbleBot")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b46021021a676d5040eef8e5788dd350fa9eb224")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("ScrabbleBot")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("ScrabbleBot")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
--- ScrabbleTemplate/obj/Debug/net7.0/ScrabbleTemplate.AssemblyInfo.fs
+++ ScrabbleTemplate/obj/Debug/net7.0/ScrabbleTemplate.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("ScrabbleTemplate")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b46021021a676d5040eef8e5788dd350fa9eb224")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7851f2084dc1e145945d7a8b662d79d0c2d576a1")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("ScrabbleTemplate")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("ScrabbleTemplate")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
