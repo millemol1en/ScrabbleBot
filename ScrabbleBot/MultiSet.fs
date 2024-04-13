dda33414ff407a67f710888ae44bc868f48a7ce5
Victor Kumar la Cour <vicl@itu.dk>
2024-04-13 12:17:24 +0200

    Implemented Assignment 4 MultisSet

--- ScrabbleBot/MultiSet.fs
+++ ScrabbleBot/MultiSet.fs
@@ -2,8 +2,59 @@
 
 module internal MultiSet
 
-    type MultiSet<'a> = Temp of unit // Not implemented
+    type MultiSet<'a when 'a : comparison> = R of Map<'a, uint32> 
 
-    let empty : MultiSet<'a> = Temp () // Not implemented
-    let add   : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a> = fun _ _ _ -> failwith "Not implemented" 
-    let fold  : ('b -> 'a -> uint32 -> 'b) -> 'b -> MultiSet<'a> -> 'b = fun _ _ _ -> failwith "Not implemented"
+    let empty : MultiSet<'a> = R Map.empty
+
+    let isEmpty (R mySet : MultiSet<'a>): bool = Map.isEmpty mySet
+
+    let size (R mySet) = 
+        Map.fold (fun acc _ elem -> acc + elem) 0u mySet
+
+    let contains (a : 'a) (R mySet) : bool =
+        Map.containsKey a mySet
+
+    let numItems (a : 'a) (R mySet : MultiSet<'a>) =
+        match Map.tryFind a mySet with
+        | None -> (uint32 0)
+        | Some value -> value
+    let add (a : 'a) (n : uint32) (R mySet : MultiSet<'a>) : MultiSet<'a> =
+        let numbers = n + (numItems a (R mySet))
+        R (Map.add a numbers mySet)
+
+    let addSingle (a : 'a) (R mySet : MultiSet<'a>) : MultiSet<'a> =
+        add a (uint32 1) (R mySet)
+
+    // val remove : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a>
+    let remove (a : 'a) (n : uint32) (R mySet  : MultiSet<'a>) : MultiSet<'a> =
+        let (numberOfOccurrences : uint32) = numItems a (R (mySet))
+        if numberOfOccurrences > n 
+            then
+                R ((Map.remove a mySet).Add(a, (numberOfOccurrences - n)))
+            else 
+                R (mySet.Remove a) 
+         //? finds the value of the given key.
+
+
+    let removeSingle (a : 'a) (mySet : MultiSet<'a>) : MultiSet<'a> = 
+        remove a 1u mySet
+
+
+    let fold (f : 'b -> 'a -> uint32 -> 'b) (x : 'b) (R mySet : MultiSet<'a>) = 
+        Map.fold f x mySet
+
+    let foldBack (f : 'a -> uint32 -> 'b -> 'b) (R mySet : MultiSet<'a>) (x : 'b) =
+        Map.foldBack f mySet x
+
+    let ofList (_ : 'a list) : MultiSet<'a> = failwith "not done"
+    let toList (_ : MultiSet<'a>) : 'a list = []
+
+
+    let map (_ : 'a -> 'b) (_ : MultiSet<'a>) : MultiSet<'b> = failwith "not done"
+
+    let union (_ : MultiSet<'a>) (_ : MultiSet<'a>) : MultiSet<'a> = failwith "not done"
+    let sum (_ : MultiSet<'a>) (_ : MultiSet<'a>) : MultiSet<'a> = failwith "not done"
+    
+    let subtract (_ : MultiSet<'a>) (_ : MultiSet<'a>) : MultiSet<'a> = failwith "not done"
+    
+    let intersection (_ : MultiSet<'a>) (_ : MultiSet<'a>) : MultiSet<'a> = failwith "not done"
