ca35dec342dfb57852515b706a7ff399e9add1fb
Victor Kumar la Cour <vicl@itu.dk>
2024-04-17 11:27:26 +0200

    Fixed 'players' line

--- ScrabbleTemplate/Program.fs
+++ ScrabbleTemplate/Program.fs
@@ -50,11 +50,13 @@ let main argv =
         
     // Uncomment this line to call your client
     
+    
     let (dictionary, time) =
         time (fun () -> ScrabbleUtil.Dictionary.mkDict words dictAPI)
 
+    let players    = [("ScrabBoyz", dictionary, ScrabBoyz.Scrabble.startGame); ("Oxyphenbutazone", dictionary, Oxyphenbutazone.Scrabble.startGame)] 
 
-    let players    = [("ScrabBoyz", dictionary, ScrabBoyz.Scrabble.startGame )]
+    
 
     //let players = spawnMultiples "OxyphenButazone" dictionary Oxyphenbutazone.Scrabble.startGame 2
 
